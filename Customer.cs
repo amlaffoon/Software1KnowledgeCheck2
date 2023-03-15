d0e275d8ec04a3de1a325754446b971f996d68eb
Anna Laffoon <ree11194@gmail.com>
2023-03-15 13:48:28 -0400

    Add project files.

---
+++ Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software1KnowledgeCheck2
+{
+    public class Customer : Person
+    {
+        public int ID { get; set; }
+        public bool Active { get; set; }
+
+        public override string ToString()
+        {
+            return $"{LastName}, {FirstName}. ID: {ID}, Active customer: {Active}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Software1KnowledgeCheck2;
+
+Console.WriteLine("How many records do you want to add? ");
+var numberOfRecords = int.Parse(Console.ReadLine());
+
+var customers = new List<Customer>();
+
+
+for (int i = 0; i < numberOfRecords; i++)
+{
+    var myClass = new Customer();
+
+    Console.WriteLine("Enter the value for the customer first name:");
+    myClass.FirstName = Console.ReadLine();
+
+    Console.WriteLine("Enter the value for the customer last name:");
+    myClass.LastName = Console.ReadLine();
+
+    Console.WriteLine("Enter the value for the customer ID:");
+    var isValidID = int.TryParse(Console.ReadLine(), out int ID);
+    if (isValidID)
+    {
+        myClass.ID = ID;
+    } 
+    else
+    {
+        Console.WriteLine("Invalid Input");
+    }
+
+    Console.WriteLine("Customer is active? True/False");
+    var isActive = bool.TryParse(Console.ReadLine(), out bool Active);
+    if (isActive)
+    {
+        myClass.Active = Active;
+    }
+
+    customers.Add(myClass);
+}
+
+foreach (var customer in customers)
+{
+Console.WriteLine(customer);
+}
